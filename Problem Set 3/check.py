87764f058564efa861b7deed44d345efc93fd94a
Chuma Meyiswa <nmeyiswa@gmail.com>
2025-03-02 10:25:07 +0200

    P3 outdated

--- Problem Set 3/check.py
+++ Problem Set 3/check.py
@@ -1,7 +1,23 @@
-fraction = input("fraction: ")
-x, y = fraction.split("/")
-x, y = int(x), int(y)
-print(x)
-print(y)
-if y < x:
-    print("Hawuuu") 
+months  = {
+    "January": 1,
+    "February": 2,
+    "March": 3,
+    "April": 4,
+    "May": 5,
+    "June": 6,
+    "July": 7,
+    "August": 8,
+    "September": 9,
+    "October": 10,
+    "November": 11,
+    "December": 12
+}
+
+
+date = "September 8, 1636"
+month, dayK, yearK = date.split(" ")
+print(month)
+print(dayK)
+print(yearK)
+
+
