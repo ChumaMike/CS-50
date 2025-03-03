f482d38177e3742a2ee6209637c4860bfbfff045
Chuma Meyiswa <nmeyiswa@gmail.com>
2025-03-03 21:10:03 +0200

    PS4 game

---
+++ Problem Set 4/game.py
@@ -0,0 +1,30 @@
+import random
+
+while True:
+    try:
+        n = input("level: ")
+        if int(n) > 0:
+            n = int(n)
+            x = random.randrange(1, n+1)
+            while True:
+                try:
+                    guess = int(input("guess int? "))
+                    if guess > 0:
+                        if guess < x:
+                            print("Too small!")
+                        
+                        elif guess > x:
+                            print("Too large")
+                        
+                        elif guess == x:
+                            print("Just right")
+                            break
+                    else:
+                        pass
+                except ValueError:
+                    pass
+            break
+        else:
+            pass
+    except ValueError:
+        pass
